067ea91e2166a5af946123cddc18c95b8b990adf
v_v_moon <vladimir.mun.47@gmail.com>
2025-04-29 14:33:42 +0500

    на паре сделали

--- buttons.py
+++ buttons.py
@@ -1,7 +1,10 @@
 from telebot import types
 
 def choice_button():
-    kb = types.ReplyKeyboardMarkup()
-    button_wiki = types.KeyboardButton('Wiki')
-    kb.add(button_wiki)
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button_wiki = types.KeyboardButton('википедия')
+    button_youtube = types.KeyboardButton('ютуб')
+    button_translate = types.KeyboardButton('переводчик')
+    button_feedback = types.KeyboardButton('feedback')
+    kb.add(button_wiki, button_youtube, button_translate, button_feedback)
     return kb
--- main.py
+++ main.py
@@ -1,41 +1,57 @@
-import telebot
+import telebot, buttons
 
 bot = telebot.TeleBot('7410629472:AAG8XXOKdSYYiwlU9q-IbH7tQmYGG3VUE8E')
 
 
-
+print("Бот запущен!")
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.from_user.id, f"Приветствую тебя {message.from_user.first_name}!")
+    bot.send_message(message.from_user.id, f"Приветствую тебя {message.from_user.first_name}!", reply_markup=buttons.choice_button())
 
 @bot.message_handler(commands=['help'])
 def help_message(message):
-    bot.send_message(message.from_user.id, "Я создан чтобы ты исправил свою жизнь!")
-
+    bot.send_message(message.from_user.id, "Я бот и у меня есть кнопки")
 
 
 @bot.message_handler(content_types=['text'])
 def text_message(message):
-    if message.text.lower() == "привет":
-        bot.send_message(message.from_user.id, "Здравствуйте!")
-
-    # разработчик это я
-    elif message.text.lower() == "разработчик":
-        bot.send_message(message.from_user.id, "https://www.instagram.com/v_v_moon?igsh=MTk0ams3ajV5Y3czNA%3D%3D&utm_source=qr")
-
-
-    # 3 кодовых слова:
-    elif message.text.lower() == "википедия":
-        bot.send_message(message.from_user.id, "https://ru.wikipedia.org/wiki/Заглавная_страница")
+    if message.text.lower() == "википедия":
+        bot.send_message(message.from_user.id, "Введите слово для поиска")
+        bot.register_next_step_handler(message, wiki)
 
     elif message.text.lower() == "ютуб":
-        bot.send_message(message.from_user.id, "https://www.youtube.com")
+        bot.send_message(message.from_user.id, "Введите слово")
+        bot.register_next_step_handler(message, youtube)
 
-    elif message.text.lower() == "что-то еще":
-        bot.send_message(message.from_user.id, "https://www.kinopoisk.ru/series/5048763/?utm_referrer=www.google.com")
-    # конец кодовых слов
+    elif message.text.lower() == "переводчик":
+        bot.send_message(message.from_user.id, "Введите слово на русском")
+        bot.register_next_step_handler(message, translate)
+
+    elif message.text.lower() == "feedback":
+        bot.send_message(message.from_user.id, "Ваш отзыв")
+        bot.register_next_step_handler(message, feedback)
+
+    elif message.text.lower() == "разработчик":
+        bot.send_message(message.from_user.id, "Разработчик: https://www.instagram.com/v_v_moon?igsh=MTk0ams3ajV5Y3czNA%3D%3D&utm_source=qr")
 
     else:
-        bot.send_message(message.from_user.id, "Бот на стадии разработки:/")
+        bot.send_message(message.from_user.id, "Бот на стадии разработки:/", reply_markup=buttons.choice_button())
+
+
+
+
+def wiki(message):
+    bot.send_message(message.from_user.id, f"https://ru.wikipedia.org/wiki/{message.text}")
+
+def translate(message):
+    bot.send_message(message.from_user.id, f"https://translate.yandex.ru/?source_lang=ru&target_lang=en&text={message.text}")
+
+def youtube(message):
+    bot.send_message(message.from_user.id, f"https://www.youtube.com/results?search_query={message.text}")
+
+def feedback(message):
+    bot.send_message(269578424, message.text)
+    bot.send_message(message.from_user.id, "Спасибо за отзыв!")
+
 
 bot.polling(none_stop=True) #позволяет боту работать 24/7
